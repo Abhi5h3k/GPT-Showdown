25494aa7e64bcbc3c81fcb30c7652143fb5ed4de
Abhi <37928721+Abhi5h3k@users.noreply.github.com>
2024-06-12 09:24:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,56 +5,83 @@ from chatgpt_bot import ChatGPTAutomation
 # from gemini_bot import GeminiAIAutomation
 import yaml
 
-# import time
+import time
 import urllib.error
+import signal
+
+# import sys
 
 # Path to your YAML file
-yaml_file_path = "propmt.yaml"
+YAML_FILE_PATH = "propmt.yaml"
 
 # Open and read the YAML file
-with open(yaml_file_path, "r") as file:
+with open(YAML_FILE_PATH, "r") as file:
     prompts = yaml.safe_load(file)
 
 # Access the prompt
 starter_prompt = prompts["prompt1"]
 alternate_prompt = prompts["prompt4"]
 
+# Define global variables to hold driver instances
+driver1 = None
+driver2 = None
+
+
+def get_chrome_driver_with_options():
+    """
+    Returns Chrome options with common settings applied.
+    """
+    options = uc.ChromeOptions()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    return uc.Chrome(options=options)
+
+
+# Set initial window sizes and positions for both drivers
+def set_window_sizes_and_positions(driver1, driver2):
+    """
+    Set initial window sizes and positions for both drivers.
+
+    Args:
+        driver1 (undetected_chromedriver.Chrome): The Selenium driver for the first browser window.
+        driver2 (undetected_chromedriver.Chrome): The Selenium driver for the second browser window.
+    """
+    # Set initial window sizes
+    WINDOW_WIDTH = 800
+    WINDOW_HEIGHT = 600
+    driver1.set_window_size(WINDOW_WIDTH, WINDOW_HEIGHT)
+    driver2.set_window_size(WINDOW_WIDTH, WINDOW_HEIGHT)
+
+    # Get screen resolution
+    screen_width = driver1.execute_script("return screen.width;")
+    screen_height = driver1.execute_script("return screen.height;")
+
+    # Calculate positions for windows
+    left_window_position = 0
+    right_window_position = screen_width / 2
+
+    driver1.set_window_size(right_window_position, screen_height)
+    driver2.set_window_size(right_window_position, screen_height)
+
+    # Position windows side by side
+    driver1.set_window_position(left_window_position, 0)
+    driver2.set_window_position(right_window_position, 0)
+
+
+def cleanup():
+    # Close the browser windows and release resources
+    print("Cleanup, Quit drivers")
+    if driver1:
+        driver1.quit()
+    if driver2:
+        driver2.quit()
+
 
 def main():
     try:
-        # url1 = "https://gemini.google.com/"
-        url2 = "https://chatgpt.com/?model=gpt-4o"
-
-        options1 = uc.ChromeOptions()
-        options1.add_argument("--no-sandbox")
-        options1.add_argument("--disable-dev-shm-usage")
-
-        driver1 = uc.Chrome(options=options1)
-
-        options2 = uc.ChromeOptions()
-        options2.add_argument("--no-sandbox")
-        options2.add_argument("--disable-dev-shm-usage")
-
-        driver2 = uc.Chrome(options=options2)
-
-        # Set initial window sizes
-        driver1.set_window_size(800, 600)
-        driver2.set_window_size(800, 600)
-
-        # Get screen resolution
-        screen_width = driver1.execute_script("return screen.width;")
-        screen_height = driver1.execute_script("return screen.height;")
-
-        # Calculate positions for windows
-        left_window_position = 0
-        right_window_position = screen_width / 2
-
-        driver1.set_window_size(right_window_position, screen_height)
-        driver2.set_window_size(right_window_position, screen_height)
-
-        # Position windows side by side
-        driver1.set_window_position(left_window_position, 0)
-        driver2.set_window_position(right_window_position, 0)
+        # URL1  = "https://gemini.google.com/"
+        URL2 = "https://chatgpt.com/?model=gpt-4o"
 
         # <=== ChatGpt ===
 
@@ -65,13 +92,13 @@ def main():
 
         print("Open GPT 1")
         # time.sleep(3)
-        driver1.get(url2)
-        # time.sleep(3)
+        driver1.get(URL2)
+        time.sleep(3)
         print("Open GPT 2")
-        driver2.get(url2)
+        driver2.get(URL2)
 
         print("=" * 20)
-        input("Hit Enter when all is set and ready to work.")
+        input("\nHit Enter when all is set and ready to work.")
         print("=" * 20)
 
         bot_1_obj.set_prompt_value(starter_prompt)
@@ -80,11 +107,35 @@ def main():
         bot_2_obj.set_prompt_value(alternate_prompt)
         bot_2_obj.start_convo_in_thread()
 
+    except KeyboardInterrupt:
+        print("\n** Exiting program due to Ctrl+C. **")
+        cleanup()
+        # sys.exit(0)
     except urllib.error.URLError as e:
-        print("URLError:", e)
+        print("URLError occurred:", e)
     except Exception as e:
-        print(e)
+        print("An unexpected error occurred:", e)
 
 
 if __name__ == "__main__":
+
+    def handle_interrupt(sig, frame):
+        print("\n** Exiting program due to Ctrl+C. **")
+        cleanup()
+        # exit(0)
+
+    signal.signal(signal.SIGINT, handle_interrupt)  # Register Ctrl+C handler
+
+    print("*" * 40)
+    print("[                                      ]")
+    print("|            GPT Showdown              |")
+    print("[          **Ctrl+C to Exit**          ]")
+    print("*" * 40)
+
+    # Initialize driver1 and driver2
+    driver1 = get_chrome_driver_with_options()
+    driver2 = get_chrome_driver_with_options()
+    set_window_sizes_and_positions(driver1, driver2)
+
+    # Call the main function
     main()
